bee68592aa23db8e2253e97db10cea20b0c4dcf5
Shakib22011 <shosaain0101@gmail.com>
2026-04-27 01:10:47 +0600

    api last

--- api/api.js
+++ api/api.js
@@ -10,15 +10,35 @@ const loadpost=() => {
     fetch(url).then(response=> response.json()).then(data=> displayPost(data));
 }
 
-const displayPost=(post)=>{
-    //get the conatiner
-    const parentContainer=document.getElementById('post-title');
-    post.forEach((each) => {
-        const li=document.createElement('li');
-        console.log(li);
-        li.innerText=each.title;
-        parentContainer.appendChild(li);
-        console.log(each);
+// const displayPost=(post)=>{
+//     //get the conatiner
+//     const parentContainer=document.getElementById('post-title');
+//     parentContainer.innerHTML='';
+//     post.forEach((each) => {
+//         const li=document.createElement('li');
+//         console.log(li);
+//         li.innerText=each.title;
+//         parentContainer.appendChild(li);
+//         console.log(each);
+//     })
+//     // console.log(post);
+// };
+
+
+const displayPost=post=>{
+    //get the parent and empty it
+    const parentContainer=document.getElementById('post-container');
+    parentContainer.innerHTML='';
+
+    //load each element
+    post.forEach(each=>{
+        const card=document.createElement('div');
+        card.innerHTML=`
+         <div class="post">
+            <h2>${each.title}</h2>
+            <p>${each.body}</p>
+        </div>`;
+        parentContainer.appendChild(card);
     })
-    // console.log(post);
-};
+}
+loadpost();
